09b7cf5b26f6ba4893c67d47372ea51182901e06
phannan <paula.hannan@oit.edu>
2018-06-27 10:34:24 -0700

    Add VS #2

---
+++ StarterCPP/starterCPP.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// starter CPP that can be used with VS
+//  to make it work with an online compiler, copy out the stdafx.h
+
+#include "stdafx.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+ 
+   
+   return 0;
+}
